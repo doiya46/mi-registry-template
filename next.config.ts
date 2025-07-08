d4efd2f645d7750681f5ecd4fc031f76d5cc874f
Mi <thangkt@fpt.com>
2025-07-08 17:23:53 +0700

    update build nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/mi-registry-template',
   trailingSlash: true,
   images: {
     unoptimized: true,
